a5ecb2ca3dd6ecac36e9ee3584a53f5010f2da62
AKod13 <akodzoman13@gmail.com>
2022-05-28 11:37:28 +0200

    password setup in rails c

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_28_065826) do
+ActiveRecord::Schema.define(version: 2022_05_28_092922) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2022_05_28_065826) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
