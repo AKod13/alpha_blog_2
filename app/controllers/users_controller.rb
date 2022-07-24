25ebbd7d72ebcbd83646a19c58cebd9c6fb23f28
AKod13 <akodzoman13@gmail.com>
2022-07-24 20:28:01 +0200

    finished categories

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
 
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
+
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
   end
